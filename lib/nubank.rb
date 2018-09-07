4900e413554b5231d7359975c13017eb0cef0336
Alan Fachini <alfakini@gmail.com>
2018-09-07 19:04:18 -0300

    Add first version of the API wrapper.
    The available methods are:
    - #account
    - #services
    - #login
    - #logout
    - #bills_summary
    - #events

--- lib/nubank.rb
+++ lib/nubank.rb
@@ -1,8 +1,111 @@
+require 'faraday'
+require 'json'
 require 'nubank/version'
 
+HEADERS = {
+  'Content-Type': 'application/json',
+  'User-Agent': 'nubank-ruby (https://www.github.com/alfakini/nubank-ruby)',
+  'Origin': 'https://conta.nubank.com.br',
+  'Referer': 'https://conta.nubank.com.br',
+}.freeze
+NUBANK_URL = 'https://prod-s0-webapp-proxy.nubank.com.br'.freeze
+
 class Nubank
+  attr_reader :conn
+
   def initialize(cpf, password)
     @cpf = cpf
     @password = password
+    @conn = Faraday.new(url: NUBANK_URL)
+    services
+  end
+
+  def account
+    JSON.parse(account_request.body)['account']
+  end
+
+  def bills_summary
+    JSON.parse(bills_summary_request.body)['bills']
+  end
+
+  def events
+    JSON.parse(events_request.body)['events']
+  end
+
+  def login
+    @login ||= JSON.parse(login_request.body)
+  end
+
+  def logout
+    logout_request
+    @conn = nil
+  end
+
+  def services
+    @services ||= JSON.parse(discovery_request.body)
+  end
+
+  def urls
+    @login['_links']
+  end
+
+  private
+
+  def account_request
+    make_get_request(urls['account']['href'])
+  end
+
+  def bills_summary_request
+    make_get_request(urls['bills_summary']['href'])
+  end
+
+  def discovery_request
+    @conn.get do |request|
+      request.url('/api/discovery')
+      request.headers = HEADERS
+    end
+  end
+
+  def events_request
+    make_get_request(urls['events']['href'])
+  end
+
+  def login_request
+    @conn.post do |request|
+      request.url(services['login'])
+      request.headers = HEADERS
+      request.body = JSON.dump({
+        grant_type: 'password',
+        login: @cpf,
+        password: @password,
+        client_id: 'other.conta',
+        client_secret: 'yQPeLzoHuJzlMMSAjC-LgNUJdUecx8XO'
+      })
+    end
+  end
+
+  def logout_request
+    make_post_request(urls['revoke_token']['href'])
+  end
+
+  def auth_headers
+    @auth_headers ||= begin
+      access_token = login['access_token']
+      HEADERS.merge('Authorization' => "Bearer #{access_token}")
+    end
+  end
+
+  def make_get_request(url)
+    @conn.get do |request|
+      request.url(url)
+      request.headers = auth_headers
+    end
+  end
+
+  def make_post_request(url)
+    @conn.post do |request|
+      request.url(url)
+      request.headers = auth_headers
+    end
   end
 end
--- spec/nubank_spec.rb
+++ spec/nubank_spec.rb
@@ -1,5 +1,169 @@
-RSpec.describe Nubank::Ruby do
+require 'nubank'
+
+RSpec.describe Nubank do
+  subject { Nubank.new('<CPF>', '<PASSWORD>') }
+
   it 'has a version number' do
-    expect(Nubank::VERSION).not_to be(nil)
+    expect(Nubank::VERSION).not_to be('0.1.0')
+  end
+
+  context '#services' do
+    it 'has services urls available' do
+      VCR.use_cassette :services do
+        expect(subject.services).to include(
+          'auth_device_resend_code',
+          'auth_gen_certificates',
+          'login',
+          'pusher_auth_channel',
+          'register_prospect',
+          'register_prospect_global_web',
+          'register_prospect_savings_mgm',
+          'register_prospect_savings_web',
+          'request_password_reset',
+          'reset_password'
+        )
+      end
+    end
+  end
+
+  context 'account api' do
+    context '#account' do
+      it 'shows account informations' do
+        VCR.use_cassette(:account) do
+          subject.login
+          expect(subject.account).to include(
+            '_links',
+            'available_balance',
+            'balances',
+            'barcode_id',
+            'cards',
+            'created_at',
+            'credit_limit',
+            'current_balance',
+            'current_interest_rate',
+            'customer_id',
+            'due_day',
+            'effective_due_date',
+            'future_balance',
+            'id',
+            'interest_rate',
+            'limit_range_max',
+            'limit_range_min',
+            'net_available',
+            'next_close_date',
+            'next_due_date',
+            'payment_method',
+            'precise_credit_limit',
+            'product_id',
+            'request_id',
+            'status',
+            'temporary_limit_amount',
+          )
+        end
+      end
+    end
+
+    context '#bills_summary' do
+      it 'shows bills informations' do
+        VCR.use_cassette(:bills_summary) do
+          subject.login
+          summary = subject.bills_summary
+          expect(summary.first).to include('_links', 'state', 'summary')
+          expect(summary.first['summary'].keys.sort).to include(
+          'adjustments',
+          'close_date',
+          'due_date',
+          'effective_due_date',
+          'expenses',
+          'fees',
+          'interest',
+          'interest_charge',
+          'interest_rate',
+          'interest_reversal',
+          'international_tax',
+          'minimum_payment',
+          'open_date',
+          'paid',
+          'past_balance',
+          'payments',
+          'precise_minimum_payment',
+          'precise_total_balance',
+          'previous_bill_balance',
+          'tax',
+          'total_accrued',
+          'total_balance',
+          'total_credits',
+          'total_cumulative',
+          'total_financed',
+          'total_international',
+          'total_national',
+          'total_payments',
+          )
+        end
+
+        VCR.use_cassette(:logout) { subject.logout }
+      end
+    end
+
+    context '#events' do
+      it 'shows all events' do
+        VCR.use_cassette(:events) do
+          subject.login
+          expect(subject.events.first).to include(
+            '_links',
+            'amount',
+            'category',
+            'description',
+            'details',
+            'href',
+            'id',
+            'time',
+            'title'
+          )
+        end
+        VCR.use_cassette(:logout) { subject.logout }
+      end
+    end
+
+    context '#login' do
+      it 'login user and make internal services available' do
+        VCR.use_cassette(:login) do
+          subject.login
+          expect(subject.urls).to include(
+            'change_password',
+            'enabled_features',
+            'certificate_status',
+            'revoke_token',
+            'userinfo',
+            'events_page',
+            'loginWebapp',
+            'dropman',
+            'events',
+            'faq_push_back_state',
+            'register_rewards',
+            'login_webapp',
+            'postcode',
+            'app_flows',
+            'rewards_signup_widget',
+            'revoke_all',
+            'customer',
+            'account',
+            'bills_summary',
+            'features_map',
+            'healthcheck',
+            'savings_account',
+            'rewards_enrollment',
+            'purchases',
+            'blackmirror',
+            'ghostflame',
+            'account_simple',
+            'shore',
+            'user_change_password'
+          )
+        end
+
+        VCR.use_cassette(:logout) { subject.logout }
+      end
+    end
   end
 end
